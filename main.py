88fc0b243830878afb8c26490236cb092b9bbbd9
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2025-04-08 18:31:20 +0530

    defolt port specified

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=SERVER_URL, port=int(PORT), reload=(ENV == "dev"))
+    uvicorn.run("main:app", host=SERVER_URL, port=int(PORT) | 4000, reload=(ENV == "dev"))
